e90bfa71d1856ccbc67c6bb213ad9971cc5c6372
Mike Smith <mike@sticknet.net>
2016-01-10 22:10:07 -0600

    Add click handler only to button

--- js/components/TodoList.react.js
+++ js/components/TodoList.react.js
@@ -5,21 +5,18 @@ export default class TodoList extends Component {
   render() {
     const { todos, onToggle } = this.props;
     const formatTime = (date) => dateFormat(date, 'ddd, mmm d')
-    const completedTime = (date) => {
-      if (date) {
-        return (<button className='date'>{formatTime(date)}</button>);
-      } else {
-        return (<button className='incomplete'>-</button>);
-      }
-    }
     return (
       <ul className='todo_list'>
         {todos.map(todo =>
-          <li key={todo.id} onClick={(e) => onToggle(todo)}>
+          <li key={todo.id}>
             <span className='date'>
               {formatTime(todo.addedAt)}
             </span>
-            {completedTime(todo.completedAt)}
+            <button
+              className={todo.completedAt ? 'date' : 'incomplete'}
+              onClick={(e) => onToggle(todo)}>
+              {todo.completedAt ? formatTime(todo.completedAt) : '-'}
+            </button>
             {todo.text}
           </li>
         )}
