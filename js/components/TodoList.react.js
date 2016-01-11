11dd9e012b9abfc863f1a0079a0cd582f11d8306
Mike Smith <mike@sticknet.net>
2016-01-10 19:16:47 -0600

    Add formatTime function

--- js/components/TodoList.react.js
+++ js/components/TodoList.react.js
@@ -4,11 +4,12 @@ import React, { Component } from 'react';
 export default class TodoList extends Component {
   render() {
     const { todos, onComplete } = this.props;
+    const formatTime = (date) => dateFormat(date, 'ddd, mmm d')
     return (
       <ul className='todo_list'>
         {todos.map(todo =>
           <li key={todo.id} onClick={(e) => onComplete(todo)}>
-            {todo.text} ({dateFormat(todo.addedAt, 'ddd, mmm d')}) ({todo.completed.toString()})
+            {todo.text} ({formatTime(todo.addedAt)}) ({todo.completed.toString()})
           </li>
         )}
       </ul>
