d5d7ee91982282c9f0aae1c1995504cccf780728
Mike Smith <mike@sticknet.net>
2016-04-30 17:23:20 -0500

    Right dropdown menu

--- js/components/TodoGroup.react.js
+++ js/components/TodoGroup.react.js
@@ -3,6 +3,12 @@ import Checkbox from 'material-ui/lib/checkbox';
 import Divider from 'material-ui/lib/divider';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
 const moment = require('moment');
 
 export default class TodoGroup extends Component {
@@ -13,18 +19,33 @@ export default class TodoGroup extends Component {
       <div>
         <List subheader={title}>
         {todos.map(todo => {
-          var secondaryText, checkbox;
-
+          var secondaryText;
           if (todo.completedAt) {
             secondaryText = formatDate(todo.completedAt);
           }
 
+          var checkbox;
           if (onToggle) {
-            checkbox = (<Checkbox
+            checkbox = <Checkbox
               defaultChecked={!!todo.completedAt}
               onCheck={() => onToggle(todo)}
-            />);
+            />;
           }
+
+          const iconButtonElement = (
+            <IconButton touch={true}>
+              <MoreVertIcon />
+            </IconButton>
+          );
+
+          const rightIconMenu = (
+            <IconMenu iconButtonElement={iconButtonElement}>
+              <MenuItem>-1 day</MenuItem>
+              <MenuItem>+1 day</MenuItem>
+              <MenuItem>Edit</MenuItem>
+              <MenuItem>Delete</MenuItem>
+            </IconMenu>
+          );
 
           return (
             <ListItem
@@ -32,6 +53,7 @@ export default class TodoGroup extends Component {
               primaryText={todo.text}
               secondaryText={secondaryText}
               leftCheckbox={checkbox}
+              rightIconButton={rightIconMenu}
             />);
         })}
         </List>
