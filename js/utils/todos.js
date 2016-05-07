76f7d7c4e607f812c65a62051b2a885e3f283565
Mike Smith <mike@sticknet.net>
2016-05-07 16:19:54 -0500

    Remove lodash from utils/todos.js

--- js/utils/todos.js
+++ js/utils/todos.js
@@ -1,9 +1,7 @@
 import { toDate } from './dates';
 
-const lodash = require('lodash');
-
-const archivedTodos = (todos) => lodash.filter(todos, (t) => t.archivedAt);
-const unarchivedTodos = (todos) => lodash.reject(todos, (t) => t.archivedAt);
+const archivedTodos = (todos) => todos.filter((t) => t.archivedAt);
+const unarchivedTodos = (todos) => todos.filter((t) => !t.archivedAt);
 
 const addedAtDate = (todo) => toDate(todo.addedAt);
 const completedAtDate = (todo) => toDate(todo.completedAt);
