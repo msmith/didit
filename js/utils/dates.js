03de314620fba4f34b463a7ed2af310deab0b378
Mike Smith <mike@sticknet.net>
2016-05-07 18:34:06 -0500

    Convert to ISOString

--- js/utils/dates.js
+++ js/utils/dates.js
@@ -6,10 +6,10 @@ const formatDate = (date) => dateFormat(date, 'ddd, mmm d');
 const toDate = (date) => {
   if (date) {
     const d = new Date(date); // ensure it's a Date type
-    // XXX - this returns an array rather than a Date because lodash.groupBy
-    //       will convert it to a string, and Dates won't sort properly.
+    // XXX - this returns a string rather than a Date because lodash.groupBy
+    //       will convert it to a string, and Date#toString() won't sort properly.
     // TODO - don't use lodash.groupBy in TodoGroups
-    return [d.getFullYear(), d.getMonth() + 1, d.getDate()];
+    return new Date(d.getFullYear(), d.getMonth(), d.getDate()).toISOString();
   }
 };
 
