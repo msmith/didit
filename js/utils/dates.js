cb3a164df475219d790c283b002a78bcad89f27a
Mike Smith <mike@sticknet.net>
2016-05-07 17:37:08 -0500

    Fix incorrect sort order

--- js/utils/dates.js
+++ js/utils/dates.js
@@ -6,7 +6,10 @@ const formatDate = (date) => dateFormat(date, 'ddd, mmm d');
 const toDate = (date) => {
   if (date) {
     const d = new Date(date); // ensure it's a Date type
-    return new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    // XXX - this returns an array rather than a Date because lodash.groupBy
+    //       will convert it to a string, and Dates won't sort properly.
+    // TODO - don't use lodash.groupBy in TodoGroups
+    return [d.getFullYear(), d.getMonth() + 1, d.getDate()];
   }
 };
 
