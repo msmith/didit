15ef5b5dbeb31fae8b5e8adc6aa7711592d44ec0
Mike Smith <mike@sticknet.net>
2016-05-07 21:17:34 -0500

    Remove lodash

--- js/components/TodoGroups.react.js
+++ js/components/TodoGroups.react.js
@@ -7,13 +7,11 @@ import TodoList from './TodoList.react';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 
-const lodash = require('lodash');
-
 export default class TodoGroups extends Component {
   render() {
     const { todos, groupBy, title, ...other } = this.props;
-    const groupedTodos = lodash.groupBy(todos, groupBy);
-    const sortedDates = lodash.sortBy(lodash.keys(groupedTodos));
+    const uniqueDates = new Set(todos.map((t) => groupBy(t)));
+    const sortedDates = Array.from(uniqueDates).sort();
     return (
       <Paper>
         {sortedDates.map(groupKey =>
@@ -21,7 +19,7 @@ export default class TodoGroups extends Component {
             <TodoList
               {...other}
               title={title(groupKey)}
-              todos={groupedTodos[groupKey]}
+              todos={todos.filter((t) => (groupBy(t) == groupKey.toString()))}
             />
             <Divider />
           </div>
--- js/utils/dates.js
+++ js/utils/dates.js
@@ -6,10 +6,7 @@ const formatDate = (date) => dateFormat(date, 'ddd, mmm d');
 const toDate = (date) => {
   if (date) {
     const d = new Date(date); // ensure it's a Date type
-    // XXX - this returns a string rather than a Date because lodash.groupBy
-    //       will convert it to a string, and Date#toString() won't sort properly.
-    // TODO - don't use lodash.groupBy in TodoGroups
-    return new Date(d.getFullYear(), d.getMonth(), d.getDate()).toISOString();
+    return new Date(d.getFullYear(), d.getMonth(), d.getDate());
   }
 };
 
